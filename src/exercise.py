6df654cf058641555728e9ee202c5460316cae51
MrSullivan <rsullivan@stcadocs.com>
2021-03-03 11:00:15 +0000

    added two variables, one with modulo conditional. if and else statements.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    number = int(input("Give a number:"))
+    remainder = number % 2
+
+    if (remainder == 0):
+      print("Number " + str(number) + " is even")
+    else:
+      print("Number " + str(number) + " is odd")
 
 if __name__ == '__main__':
     main()
